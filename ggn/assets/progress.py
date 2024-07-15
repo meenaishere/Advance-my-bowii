c10624f6ca00ec868e21aaa7f249b5c50da288f6
meenaishere <herokugithub663@gmail.com>
2024-07-15 15:46:38 +0530

    Update progress.py

--- ggn/assets/progress.py
+++ ggn/assets/progress.py
@@ -5,8 +5,8 @@ import json
 from ggn.assets.functions import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "🟢"
-UN_FINISHED_PROGRESS_STR = "🔴"
+FINISHED_PROGRESS_STR = "☠️"
+UN_FINISHED_PROGRESS_STR = "🤡"
 DOWNLOAD_LOCATION = "/app"
 
 

4d38ff21d23a7cfdfe9859fd6a7513602b4775fc
meenaishere <herokugithub663@gmail.com>
2024-08-06 17:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,11 @@
 
 from os import getenv
 
+API = environ.get("API", "") # shortlink api
+URL = environ.get("URL", "") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")

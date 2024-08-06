c449316a53a5687e1d31f5e1b00470c86b07c576
meenaishere <herokugithub663@gmail.com>
2024-08-06 18:23:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API = environ.get("API", "") # shortlink api
-URL = environ.get("URL", "") # shortlink domain without https://
+API = environ.get("API", "cbe405a9e2a6f851dcb0513402375f6bf60c2c4c") # shortlink api
+URL = environ.get("URL", "https://kingurl.in/") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "Gshsjsosch_bot") # bot username without @
 VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
